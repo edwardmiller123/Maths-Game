f79e5e19e66f6f4158bc537d750ca2543ac6e747
edwardmiller123 <edwardomiller123@gmail.com>
2022-06-10 22:10:33 +0100

    initialised repo

---
+++ mathsGame.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main() {
+    char start;
+    while (start != 's') {
+        printf("Press s to start\n");
+        scanf("%c", &start);
+    }
+    int t_start = clock();
+    int i = 0;
+    while (i < 10) {
+        int num1, num2, answer;
+        answer = -1;
+        while (num1 + num2 != answer) {
+            srand(time(0));
+            num1 = rand() % 11;
+            num2 = rand() % 11;
+            printf("%i + %i = ? \n", num1, num2);
+            scanf("%i", &answer);
+        }
+        i++;
+
+    }
+    int t_end = clock();
+    int t_total = t_end - t_start;
+    printf("Game Finished. Your time was %i", t_total);
+}
